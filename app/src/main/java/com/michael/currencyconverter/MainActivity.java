8b80af5031e48e8d1550aa725880a1c6ee9e954c
Michaelzy27 <55640053+Michaelzy27@users.noreply.github.com>
2020-06-14 13:29:10 +0100

    Initial commit

---
+++ app/src/main/java/com/michael/currencyconverter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.michael.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private void click(View view) {
+
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
